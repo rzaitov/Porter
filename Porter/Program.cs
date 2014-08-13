47ea6ce5c953a5f138bb749dd6dbefc7a07372c5
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-08-13 21:57:50 +0400

    Initial commit

---
+++ Porter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Porter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string path = args [0];
+			string sourceCode = File.ReadAllText (path);
+
+			SourceAnalayzer engine = new SourceAnalayzer ();
+			string methodList = engine.GetMethodsFrom (sourceCode);
+
+			Console.WriteLine (methodList);
+		}
+	}
+}
---
+++ Porter/SourceAnalayzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Porter
+{
+	public class SourceAnalayzer
+	{
+		public SourceAnalayzer ()
+		{
+		}
+
+		public string GetMethodsFrom(string objcSource)
+		{
+			var methods = new List<string> ();
+
+			string[] lines = objcSource.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+			foreach (var line in lines) {
+				if (IsMethodDeclaration (line))
+					methods.Add (line);
+			}
+
+			return string.Join (Environment.NewLine, methods);
+		}
+
+		private bool IsMethodDeclaration(string line)
+		{
+			string l = line.Trim ();
+
+			return l.StartsWith ("+") || l.StartsWith ("-");
+		}
+	}
+}
+
